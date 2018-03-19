f64fed0324eb2eb04d53f16c4d988b33be914c9e
AHoobler <ashley.hoobler@gmail.com>
2018-03-19 18:01:35 -0400

    used on scroll

--- DOMPracticeSite/JavaScript.js
+++ DOMPracticeSite/JavaScript.js
@@ -1,1 +1,10 @@
-﻿
+﻿window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("greenP").className = "scrollgreen";
+    }
+    else {
+        document.getElementById("greenP").className = "";
+    }
+}
